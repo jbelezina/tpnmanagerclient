0259d0ec76c307ec49e7cd2b302b8a61c7d2390d
Jan Belezina (PGS Software) <jbelezina@pgs-soft.com>
2018-03-23 23:27:22 +0100

    FoodDrinkForm component improvements

--- src/FoodDrinkForm.js
+++ src/FoodDrinkForm.js
@@ -3,26 +3,74 @@ import './css/Forms.css';
 import './css/reactDateTime.css';
 import Datetime from 'react-datetime';
 import moment from 'moment';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 import 'moment/locale/pl';
 
 class FoodDrinkForm extends Component {  
     constructor(){
       super();
       this.state = {
-        product: '',
+        foodSelect: ['active','','',''],
+        drinkSelect: ['active','','',''],
+        drink: '',
+        food: '',
         portion: '',
         stop: '',
         start: 'podaj łączną długość przyjmowania',
         comment: '',
       }
       this.handleDatePicker = this.handleDatePicker.bind(this);
-      this.handleProduct = this.handleProduct.bind(this);
+      this.handleFoodSize = this.handleFoodSize.bind(this);
+      this.handleDrinkSize = this.handleDrinkSize.bind(this);
+      this.handleDrinkSelect = this.handleDrinkSelect.bind(this);
+      this.handleFoodSelect = this.handleFoodSelect.bind(this);
     }
 
-    handleProduct(e){
-       this.setState({product: e.target.value});
-       console.log(e.target.value);
+    handleFoodSize(index){
+      let oldFoodSelectState = this.state.foodSelect;
+      let newFoodSelectState = oldFoodSelectState.map((oldstate, i)=>{
+        if (i === index) {
+          return 'active';
+        } else {
+          return '';
+        }
+      });
+      console.log('newFoodSelectState', newFoodSelectState);
+      this.setState({foodSelect:newFoodSelectState});
     }
+    
+    handleDrinkSize(index){
+      let oldDrinkSelectState = this.state.drinkSelect;
+      let newDrinkSelectState = oldDrinkSelectState.map((oldstate, i)=>{
+        if (i === index) {
+          return 'active';
+        } else {
+          return '';
+        }
+      });
+      console.log('newDrinkSelectState', newDrinkSelectState);
+      this.setState({drinkSelect:newDrinkSelectState});
+    }
+
+    handleDrinkSelect(selected){
+      if (selected){
+        this.setState({drink:selected.value})
+      } else {
+        this.setState({drink:''});
+      }
+    }
+
+    handleFoodSelect(selected){
+      console.log(selected.value);
+      if (selected){
+        this.setState({food:selected.value})
+      } else {
+        this.setState({food:''});
+      }
+    }
+
+    
 
     handleDatePicker(mom){
       let myString = moment(mom).format("YYYY-MM-DD HH:mm:ss"); 
@@ -31,9 +79,18 @@ class FoodDrinkForm extends Component {
     }
 
     render(){
-      let style = {
-        width:"100%",
-      }
+      
+      const foodOptions = [
+        { value: 'Kluski z makiem', label: 'Kluski z makiem' },
+        { value: 'Kotlety mielone z ziemniakami', label: 'Mielone z ziemniakami' },
+        { value: 'Kulesza', label: 'Kulesza' },
+      ];
+      
+      const drinkOptions = [
+        { value: 'Woda', label: 'Woda' },
+        { value: 'Herbata', label: 'Herbata' },
+        { value: 'Wino czerwone', label: 'Wino czerwone' },
+      ];
 
       let iconArea = {
         height: "100%",
@@ -71,20 +128,24 @@ class FoodDrinkForm extends Component {
                   <div className="form-group row m-3">                  
                     <label className="col-sm-2 col-form-label">Jedzenie:</label>
                     <div className="col-sm-8">
-                      <select onChange={this.handleProduct} value={this.state.product} ref="product" className="form-control" id="exampleFormControlSelect1">
-                        <option>Makaron</option>
-                        <option>Ser</option>
-                      </select>
+                      <Select
+                        menuContainerStyle={{ zIndex: '2' }}
+                        name="jedzenie"
+                        value={this.state.food}
+                        options={foodOptions}
+                        onChange={this.handleFoodSelect}
+                        placeholder="Wybierz jedzenie"
+                      />
                     </div>
                   </div>
                   <div className="form-group row m-3">  
                   <label className="col-sm-2 col-form-label">Porcja:</label>
-                    <div className="col-10">
-                      <div className="btn-group" role="group" aria-label="...">
-                        <button style={{width:'20%'}} type="button" className="btn btn-secondary">S</button>
-                        <button style={{width:'20%'}} type="button" className="btn btn-secondary">M</button>
-                        <button style={{width:'20%'}} type="button" className="btn btn-secondary">L</button>
-                        <button style={{width:'20%'}} type="button" className="btn btn-secondary">XL</button>
+                    <div className="col-8">
+                      <div className="btn-group btn-group-lg btn-group-justified" role="group" aria-label="buttons">
+                        <button type="button" onClick={()=>{this.handleFoodSize(0)}} className={"btn btn-default" + ' ' + this.state.foodSelect[0]}>S</button>
+                        <button type="button" onClick={()=>{this.handleFoodSize(1)}} className={"btn btn-default" + ' ' + this.state.foodSelect[1]}>M</button>
+                        <button type="button" onClick={()=>{this.handleFoodSize(2)}} className={"btn btn-default" + ' ' + this.state.foodSelect[2]}>L</button>
+                        <button type="button" onClick={()=>{this.handleFoodSize(3)}} className={"btn btn-default" + ' ' + this.state.foodSelect[3]}>XL</button>
                       </div>
                     </div>
                   </div>
@@ -92,20 +153,24 @@ class FoodDrinkForm extends Component {
                   <div className="form-group row m-3">
                     <label className="col-sm-2 col-form-label">Picie:</label>
                     <div className="col-sm-8">
-                      <select onChange={this.handleProduct} value={this.state.product} ref="product" className="form-control" id="exampleFormControlSelect1">
-                        <option>Makaron</option>
-                        <option>Ser</option>
-                      </select>
+                      <Select
+                        menuContainerStyle={{ zIndex: '2' }}
+                        name="university"
+                        value={this.state.drink}
+                        options={drinkOptions}
+                        onChange={this.handleDrinkSelect}
+                        placeholder="Wybierz napój"
+                      />
                     </div>
                   </div>
                   <div className="form-group row m-3">  
                   <label className="col-sm-2 col-form-label">Porcja:</label>
-                    <div className="col-sm-10">
-                      <div className="btn-group" role="group">
-                      <button type="button" className="btn btn-secondary">S</button>
-                      <button type="button" className="btn btn-secondary">M</button>
-                      <button type="button" className="btn btn-secondary">L</button>
-                      <button type="button" className="btn btn-secondary">XL</button>
+                    <div className="col-8">
+                      <div className="btn-group btn-group-lg btn-group-justified" role="group" aria-label="buttons">
+                        <button ref='0' type="button" onClick={()=>{this.handleDrinkSize(0)}} className={"btn btn-default" + ' ' + this.state.drinkSelect[0]}>S</button>
+                        <button ref='1' type="button" onClick={()=>{this.handleDrinkSize(1)}} className={"btn btn-default" + ' ' + this.state.drinkSelect[1]}>M</button>
+                        <button ref='2' type="button" onClick={()=>{this.handleDrinkSize(2)}} className={"btn btn-default" + ' ' + this.state.drinkSelect[2]}>L</button>
+                        <button ref='3' type="button" onClick={()=>{this.handleDrinkSize(3)}} className={"btn btn-default" + ' ' + this.state.drinkSelect[3]}>XL</button>
                       </div>
                     </div>
                   </div>
@@ -123,7 +188,7 @@ class FoodDrinkForm extends Component {
                     </div>
                   </div>
                   <div className="form-group row m-3">
-                    <button type="button" className="pr-3 mt-3 col-12 btn btn-dark">Dodaj</button>
+                    <button type="button" className="pr-3 mt-4 mb-4 col-12 btn btn-dark">Dodaj</button>
                   </div>
                 </form>
               </div>
